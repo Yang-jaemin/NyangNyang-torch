73135636390f50ab3a8e4d51383fe6df3cff9bb1
Yang-jaemin <108872973+Yang-jaemin@users.noreply.github.com>
2023-08-19 02:34:25 +0900

    Feat: data_utils

--- dataloader.py
+++ dataloader.py
@@ -1,19 +1,23 @@
 import random
 
-class DataLoader:
+class Nyang_DataLoader:
     def __init__(self, dataset, batch_size=1, shuffle=False):
         self.dataset = dataset
         self.batch_size = batch_size
         self.shuffle = shuffle
-        self.indices = list(range(len(dataset))) 
+        self.indexs = list(range(len(dataset))) 
 
     def __iter__(self):
         if self.shuffle:
-            random.shuffle(self.indices)
-
+            random.shuffle(self.indexs)
         for start_idx in range(0, len(self.dataset), self.batch_size):
             end_idx = min(start_idx + self.batch_size, len(self.dataset))
-            yield [self.dataset[idx] for idx in self.indices[start_idx:end_idx]]  # 제너레이터
+            batch = [self.dataset[idx] for idx in self.indexs[start_idx:end_idx]]
+
+            images = [sample[0] for sample in batch]
+            labels = [sample[1] for sample in batch]
+
+            yield images, labels  # 제너레이터 쓴 이유 : 배치로 뱉어주고 다음배치는 end_idx부터 시작해야되니까
 
     def __len__(self):
         return (len(self.dataset) + self.batch_size - 1) // self.batch_size
